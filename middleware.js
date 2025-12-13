8e847c99ddcc03d94a5e344182977235d95d886c
logannsana-pixel <logannsana@gmail.com>
2025-12-13 01:13:07 +0100

    Create middleware.js
    it's for rooting to subdomain

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const url = req.nextUrl.clone();
+  const hostname = req.headers.get("host") || "";
+
+  // ADMIN PANEL
+  if (hostname === "admin-panel.kalimero.shop") {
+    if (!url.pathname.startsWith("/admin")) {
+      url.pathname = `/auth/admin${url.pathname}`;
+      return NextResponse.rewrite(url);
+    }
+  }
+  return NextResponse.next();
+}
